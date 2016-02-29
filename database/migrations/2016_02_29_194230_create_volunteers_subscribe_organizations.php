1d48ed37ab0a275da95f338a918ee6e50313ce56
Mohamed Elzarei <mohamedelzarei@gmail.com>
2016-02-29 22:17:05 +0200

    Added Cascade on deletion.

--- database/migrations/2016_02_29_194230_create_volunteers_subscribe_organizations.php
+++ database/migrations/2016_02_29_194230_create_volunteers_subscribe_organizations.php
@@ -13,10 +13,12 @@ class CreateVolunteersSubscribeOrganizations extends Migration
     public function up()
     {
         Schema::create('volunteers_subscribe_organizations', function (Blueprint $table) {
-            $table->integer('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->integer('organization_id')->unsigned();
-            $table->foreign('organization_id')->references('id')->on('organizations');
+
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->integer('organization_id')->unsigned()->index();
+            $table->foreign('organization_id')->references('id')->on('organizations')->onDelete('cascade');
 
             $table->timestamps();
         });
