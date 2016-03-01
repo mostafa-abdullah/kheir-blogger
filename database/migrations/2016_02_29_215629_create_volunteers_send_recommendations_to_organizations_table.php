eec36e742730499527629708651abc7ffc8ce24c
Omar Radwan <ommarradwan213@gmail.com>
2016-03-01 18:05:08 +0200

    Recommendations migration fixed

--- database/migrations/2016_02_29_215629_create_volunteers_send_recommendations_to_organizations_table.php
+++ database/migrations/2016_02_29_215629_create_volunteers_send_recommendations_to_organizations_table.php
@@ -16,16 +16,13 @@ class CreateVolunteersSendRecommendationsToOrganizationsTable extends Migration
         {
             $table->increments('id');
             $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->integer('organization_id')->unsigned();
+            $table->foreign('organization_id' , 'org_id')->references('id')->on('organizations')->onDelete('cascade');
             $table->longText('recommendation');
             $table->timestamps();
         });
 
-        Schema::table('volunteers_send_recommendations_to_organizations', function(Blueprint $table)
-        {
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('organization_id' , 'org_id')->references('id')->on('organizations')->onDelete('cascade');
-        });
     }
 
     /**
