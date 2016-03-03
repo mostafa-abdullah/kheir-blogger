7e737c48a07f1c670ad0af91e89d726ff4edc65c
hossamElfar <hossam.elfar@student.guc.edu.eg>
2016-03-03 16:09:46 +0300

    fixed an error that keep showing in event.blade.php

--- resources/views/events/event.blade.php
+++ resources/views/events/event.blade.php
@@ -1,9 +1,10 @@
-@extends('layouts.app)
+@extends('layouts.app')
 @section('content')
     <div class="container">
        <h1>
            Event {{$event->name}}
        </h1>
+        <h2>{{$event->timing->diffForHumans()}}</h2>
         <h3>Description</h3>
         <p>
             {{$event->description}}
