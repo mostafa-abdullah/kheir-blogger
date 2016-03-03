6d6c394b5494cae18e5f4e566daad5037b68381e
hossamElfar <hossam.elfar@student.guc.edu.eg>
2016-03-03 15:36:09 +0300

    created the event view page

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Event;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
 class EventController extends Controller
 {
-    //
+    public function show($id)
+    {
+        $event = Event::findOrFail($id);
+        return view('events.event', compact('event'));
+    }
 
 }
