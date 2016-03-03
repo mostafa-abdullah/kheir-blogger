ad48441defcbe5c1e8c27eff5e9009972833720c
hossamElfar <hossam.elfar@student.guc.edu.eg>
2016-03-03 16:54:09 +0300

    Added some documentation to the show method

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -9,6 +9,11 @@ use App\Http\Requests;
 
 class EventController extends Controller
 {
+    /**
+     * show method to show an event 
+     * @param $id : the id of a certain event
+     * @return A view to show a certain event
+     */
     public function show($id)
     {
         $event = Event::findOrFail($id);
