c6c1dbab89d2eb053b2c33c2c04496befbad59dd
hossamElfar <hossam.elfar@student.guc.edu.eg>
2016-03-03 15:47:27 +0300

    added the timing attribute to the dates field and pass the event contents to the event view

--- resources/views/events/event.blade.php
+++ resources/views/events/event.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app)
 @section('content')
     <div class="container">
-        
+       <h1>
+           Event {{$event->name}}
+       </h1>
+        <h3>Description</h3>
+        <p>
+            {{$event->description}}
+        </p>
     </div>
 @stop
