8d1360a0735b04617582d2d56984f4fc77e37d3b
joe3141 <youssefzidan.yz@gmail.com>
2016-03-13 21:25:52 +0200

    added the methods related to SX21 to the Question model

--- app/Question.php
+++ app/Question.php
@@ -10,21 +10,48 @@ class Question extends Model
 	*	Attributes to be mass filled.
 	*/
     protected $fillable = [
-        'user_id', 'organization_id', 'question', 'answer'
+        'user_id', 'organization_id', 'event_id', 'question', 
+        'question_body', 'answer', 'answered_at'
     ];
+    /**
+    *	Attribute(s) not to be mass filled.
+    */
+    protected $guarded = array('id');
 
     /**
-    *	Fetches the questions that would be presented in the events page.
+    *	A question belongs to a single event.
     */
-    public function index()
+    public function event()
     {
-    	
+    	return $this->belongsTo('Event');
     }
 
-    public function show($id)
+    /**
+    *	Fetches the answered questions that would be presented in the events page.
+    */
+    public function showQuestions()
     {
-    	
+    	// may be changed to take(10) depending on the layout of the 'event' view.
+    	return DB::table('questions')
+    					->whereNotNull('answer')
+    					->get();
     }
 
+    /**
+	*	Fetches one answered question, typically used in creating hyper links.
+	*/
+    public function showQuestion($id)
+    {
+    	return Question::find($id);
+    }
 
+    /**
+    *	Adds a new question to the database by taking individual attributes.
+    */
+    public function askQuestion($user_id, $organization_id, $event_id,
+    	$question, $question_body)
+    {
+    	Question::create(array('user_id' => $user_id, 'organization_id' => $organization_id, 'event_id' => $event_id, 'question' => $question,
+    		'question_body' => $question_body));
+    }
 }
