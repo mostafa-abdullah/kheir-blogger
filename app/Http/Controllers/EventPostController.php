a79f23fb024b83e04e4337ccdcfc7427a183aaa7
Mohamed El Zarei <mohamedelzarei@gmail.com>
2016-03-19 21:43:52 +0200

    Validation for form Input.

--- app/Http/Controllers/EventPostController.php
+++ app/Http/Controllers/EventPostController.php
@@ -5,16 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Http\Requests\EventPostRequest as EventPostRequest;
 
 class EventPostController extends Controller
 {
     public function createPost()
     {
-    	return view('post.createPost');
+    	return view('post.createPost',compact($id));
     }
 
-    public function storePost()
+    public function storePost(EventPostRequest $request)
     {
-    	
+    	echo $request->get('title');
+    	echo $request->get('description');
+
     }
 }
--- resources/views/post/createPost.blade.php
+++ resources/views/post/createPost.blade.php
@@ -2,18 +2,36 @@
 @section('content')
 {!! Form::open(['action' => 'EventPostController@storePost']) !!}
 
-<div class="form-group">
-{!! Form::label("Title") !!}
+
+
+<div class="form-group" style="margin-left: 20px;">
+<h1> Create New Post </h1>
+</div>
+
+	@if($errors->has())
+		<div class="alert alert-danger" style="width: 50%;margin-left: 20px;">
+		<ul>
+			<h3>Errors</h3>
+		    @foreach($errors->all() as $error)
+		        <li class="warning">{{ $error }}</li>
+		    @endforeach
+		</ul>
+		</div>
+	@endif
+
+<div class="form-group" style="margin-left: 20px;">
+{!! Form::label("Title") !!} <br />
 {!! Form::text("title") !!}
 </div>
 
-<div class="form-group">
-{!! Form::label("Description") !!}
+<div class="form-group" style="margin-left: 20px;">
+{!! Form::label("Description") !!} <br />
 {!! Form::textarea("description") !!}
 </div>
 
-<div class="form-group">
+<div class="form-group" style="margin-left: 20px;">
 {!! Form::submit("Publish") !!}
 {!! Form::close() !!}
 </div>
+
 @endsection
