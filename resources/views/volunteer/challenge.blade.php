db61ea913ea4668586d72ffa1a1babb353380b2e
Omar Radwan <ommarradwan213@gmail.com>
2016-03-21 23:27:32 +0200

    edit the create challenge view

--- resources/views/volunteer/challenge.blade.php
+++ resources/views/volunteer/challenge.blade.php
@@ -8,8 +8,8 @@
     {!! Form::open(['action' => ['VolunteerController@storeChallenge' , $id]]) !!}
 
     <div class = "form-group">
-        {!! Form::label('challenge' , 'How many events will you attend this year?') !!}
-        {!! Form::number('challenge' , '0' , ['class' => 'form-control']) !!}
+        {!! Form::label('events' , 'How many events will you attend this year?') !!}
+        {!! Form::number('events' , '0' , ['class' => 'form-control']) !!}
     </div>
 
     <div>
