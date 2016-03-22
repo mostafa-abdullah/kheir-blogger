4ec439dcae4d15f4fac7082d9bf89b186702de88
AhmadElsagheer <ahmad.elsagheer@yahoo.com>
2016-03-22 20:46:20 +0200

    removed notifications controller

--- app/Notification.php
+++ app/Notification.php
@@ -16,6 +16,14 @@ class Notification extends Model
          return $this->belongsToMany('App\Event')->withTimestamps();
      }
 
+     /**
+      *  Users can receive many notifications
+      */
+     public function users(){
+
+         return $this->belongsToMany('App\User', 'user_notifications')
+                     ->withTimestamps()->withPivot('read');
+     }
 
     /**
      * Get all the not read notifications.
@@ -37,50 +45,6 @@ class Notification extends Model
         return $query->where('read', '=', '1');
     }
 
-    /**
-     *  Users can receive many notifications
-     */
-    public function users(){
-
-        return $this->belongsToMany('App\User', 'user_notifications')
-                    ->withTimestamps()->withPivot('read');
-    }
-
-    /**
-     *	Send a notification to the specified list of users
-     */
-    public function addNotification($inputUsersArray , $event , $Description , $link)
-    {
-
-
-        /**
-         * creating new notification and insert it into notification table
-         */
-        $date_time = Carbon::now();
-        $newNotificaton = new Notification;
-        $newNotificaton = [
-            'date_time' => $date_time,
-            'description' => $Description,
-            'link' => $link
-
-        ];
-        $newNotificaton->save();
-
-        /**
-         *  insert record into event_notification table which is the pivot table of many to many relationship between notifications and events
-         */
-        $lastInsertedNotificationID = $newNotificaton->id;
-        $event->notifications()->attach($lastInsertedNotificationID);
-
-        /**
-         * insert record into user_notification table which is the pivot table of many to many relationship between notifications and users
-         */
-
-        foreach ($inputUsersArray as $user) {
-            $user->notifications()->attach($lastInsertedNotificationID, ['read' => 0]);
-        }
-    }
-
     public static function notify($usersToNotify, $event, $description, $link){
 
         $notification = new Notification;
@@ -89,6 +53,6 @@ class Notification extends Model
         $event->notifications()->save($notification);
 
         foreach($usersToNotify as $user)
-          $user->notifications()->attach($notification, ['read' =>0]);
+          $user->notifications()->attach($notification, ['read' => 0 ]);
     }
 }
