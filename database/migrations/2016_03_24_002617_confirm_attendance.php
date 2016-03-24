f16088965e2cb9a74c25bfcaf3db8f5da9f950ac
baher10 <engineerbaher10@gmail.com>
2016-03-24 16:17:48 +0200

    cancel_event user story

---
+++ database/migrations/2016_03_24_002617_confirm_attendance.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ConfirmAttendance extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
