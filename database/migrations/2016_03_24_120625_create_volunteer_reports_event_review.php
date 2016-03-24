a8125dc22638b6064fb17da8d0434a85e6c053c6
soofyhbk2007 <mastergame_2007@hotmail.com>
2016-03-24 14:10:35 +0200

    created migrations

--- database/migrations/2016_03_24_120625_create_volunteer_reports_event_review.php
+++ database/migrations/2016_03_24_120625_create_volunteer_reports_event_review.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateVolunteerReportReview extends Migration
+class CreateVolunteerReportsEventReview extends Migration
 {
     /**
      * Run the migrations.
@@ -12,12 +12,14 @@ class CreateVolunteerReportReview extends Migration
      */
     public function up()
     {
-        Schema::create('volunteer_report_review', function (Blueprint $table) {
+        Schema::create('event_review_reports', function (Blueprint $table) {
+            $table->increments('id');
             $table->integer('user_id')->unsigned()->index();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
 
             $table->integer('review_id')->unsigned()->index();
-            $table->foreign('review_id')->references('id')->on('reviews')->onDelete('cascade');
+            $table->foreign('review_id')->references('id')->on('event_reviews')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
@@ -29,6 +31,6 @@ class CreateVolunteerReportReview extends Migration
      */
     public function down()
     {
-        Schema::drop('volunteer_report_review');
+        Schema::drop('event_review_reports');
     }
 }
---
+++ database/migrations/2016_03_24_120647_create_volunteer_reports_organization_review.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVolunteerReportsOrganizationReview extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organization_review_reports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->integer('review_id')->unsigned()->index();
+            $table->foreign('review_id')->references('id')->on('organization_reviews')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('organization_review_reports');
+    }
+}
