4193a627c4f03b2c2cae6bf3ea8c81e8f71fe959
baher10 <engineerbaher10@gmail.com>
2016-03-25 19:10:58 +0200

    cancel event functio

--- app/Event.php
+++ app/Event.php
@@ -3,10 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+   use Illuminate\Database\Eloquent\SoftDeletes;
 class Event extends Model
 {
-
+ use SoftDeletes;
     protected $fillable = [
         'name', 'description', 'timing', 'location',
         'required_contact_info','needed_membership'
