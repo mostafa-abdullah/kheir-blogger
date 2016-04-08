b25540d1daa3da3963cbbb4f34eab700046b9e08
AhmedSoliman7 <AhmedSoliman7@github.com>
2016-04-08 16:39:51 +0200

    #69 added AdminController till merging khlly's one

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Organization;
+use Illuminate\Http\Request;
+use App\Http\Requests\VolunteerRequest;
+
+use App\User;
+use App\Event;
+use App\Challenge;
+use App\Feedback;
+
+use Carbon\Carbon;
+use Auth;
+
+class AdminController  extends Controller{
+
+  
+}
