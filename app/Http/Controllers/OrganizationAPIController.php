815b306842a297070ab68c40bb94b482cf7fc331
Omar Radwan <ommarradwan213@gmail.com>
2016-04-10 14:42:24 +0200

    #80 implement showList function

--- app/Http/Controllers/OrganizationAPIController.php
+++ app/Http/Controllers/OrganizationAPIController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Organization;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
 class OrganizationAPIController extends Controller
 {
-    //
+
+    /**
+     *  get json list of all organizations
+     */
+    public function showList()
+    {
+        $organizations = Organization::all();
+        return response()->json($organizations);
+    }
+
 }
