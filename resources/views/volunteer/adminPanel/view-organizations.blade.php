21dce227d1ab4386e0aea127ee9964225a84e1f2
AhmedSoliman7 <AhmedSoliman7@github.com>
2016-04-18 15:23:20 +0200

    #69 added table columns (subscribers , events , cancelled events, rate) to the view

--- resources/views/volunteer/adminPanel/view-organizations.blade.php
+++ resources/views/volunteer/adminPanel/view-organizations.blade.php
@@ -12,16 +12,24 @@
             <th>Slogan</th>
             <th>Phone</th>
             <th>Location</th>
+            <th>Number of subscribers</th>
+            <th>Events held</th>
+            <th>Events cancelled</th>
+            <th>Rate</th>
         </tr>
     </thead>
     <tbody>
       @foreach ($organizations as $organization)
       <tr>
-          <td><a href="/organization/{{$organization->id}}/show">{{$organization->name}}</a></td>
+          <td><a href="/organization/{{$organization->id}}">{{$organization->name}}</a></td>
           <td>{{$organization->email}}</td>
           <td>{{$organization->slogan}}</td>
           <td>{{$organization->phone}}</td>
           <td>{{$organization->location}}</td>
+          <td>{{$organization->numberOfSubscribers}}</td>
+          <td>{{$organization->numberOfEvents}}</td>
+          <td>{{$organization->numberOfCancelledEvents}}</td>
+          <td>{{$organization->rate}}</td>
       </tr>
       @endforeach
     </tbody>
