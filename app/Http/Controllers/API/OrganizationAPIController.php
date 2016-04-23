9458a22e2bcc31638ebdb874e21c2f10fa1acad2
omarradwan <omarradwan213@gmail.com>
2016-04-23 14:08:36 +0200

    #89 implement all functions in OrganizationAPIController and call the corresponding functions in the services

--- app/Http/Controllers/API/OrganizationAPIController.php
+++ app/Http/Controllers/API/OrganizationAPIController.php
@@ -3,14 +3,37 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-
+use App\Http\Requests\RecommendationRequest;
+use App\Http\Services\OrganizationService;
 use App\Organization;
 use Illuminate\Http\Request;
 
-use App\Http\Requests;
+use App\Http\Requests\OrganizationRequest;
+
 
 class OrganizationAPIController extends Controller
 {
+
+    private $organizationService;
+
+    public function __construct()
+    {
+        $this->organizationService = new OrganizationService();
+
+        $this->middleware('auth_volunteer', ['only' => [
+            'subscribe', 'unsubscribe',
+            'recommend', 'storeRecommendation',
+            'block', 'unblock'
+        ]]);
+
+        $this->middleware('auth_organization', ['only' => [
+            'edit', 'update', 'viewRecommendations'
+        ]]);
+
+        $this->middleware('auth_admin', ['only' => [
+            'destroy',
+        ]]);
+    }
 
     /**
      *  get json list of all organizations
@@ -18,7 +41,7 @@ class OrganizationAPIController extends Controller
     public function index()
     {
         $organizations = Organization::all();
-        return $organizations;
+        return response()->json($organizations);
     }
 
 
@@ -31,7 +54,57 @@ class OrganizationAPIController extends Controller
         $organization->events = $organization->events()->get();
         $organization->reviews = $organization->reviews()->get();
         $organization->subscribers = $organization->subscribers()->get();
-        return $organization;
+        return response()->json($organization);
     }
+
+    /**
+     *  update organization information
+     */
+    public function update(OrganizationRequest $request , $id)
+    {
+        $this->organizationService->update($request , $id);
+    }
+
+    /**
+     *  volunteer subscribe to a certain organization
+     */
+    public function subscribe($id)
+    {
+        $this->organizationService->subscribe($id);
+    }
+
+    /**
+     *  volunteer unsubscribe to a certain organization
+     */
+    public function unsubscribe($id)
+    {
+        $this->organizationService->unsubscribe($id);
+    }
+
+
+    public function block($organization_id)
+    {
+        $this->organizationService->block($organization_id);
+    }
+
+
+    public function unblock($organization_id)
+    {
+        $this->organizationService->unblock($organization_id);
+    }
+
+
+    public function storeRecommendation(RecommendationRequest $request, $id)
+    {
+        $this->organizationService->storeRecommendation($request, $id);
+    }
+
+
+    public function viewRecommendations($id)
+    {
+        $recommendations = $this->organizationService->viewRecommendations($id);
+        return response()->json($recommendations);
+    }
+
 
 }
