91b6143d78053f485307de135ebd87b542b6e878
HatemMorgan <hatemmorgan17@gmail.com>
2016-04-23 07:13:34 +0200

    try

--- config/app.php
+++ config/app.php
@@ -156,7 +156,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\ElasticSearchServiceProvider::class,
+     
 
        // Cviebrock\LaravelElasticsearch\ServiceProvider::class,
 
