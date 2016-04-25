fafdb3b89a61065a6e8d18a62cbcf8d63eb28207
AhmadElsagheer <ahmad.elsagheer@yahoo.com>
2016-04-25 07:38:10 +0200

    #60 | added artisan commands for creating elastic search indices

---
+++ app/Console/Commands/ElasticSearchCreateIndex.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use App\Elastic\Elastic as Elasticsearch;
+use Elasticsearch\ClientBuilder as elasticClientBuilder;
+
+class ElasticSearchCreateIndex extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'elastic:create {index name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new index to be added to elastic search';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $client = new Elasticsearch(elasticClientBuilder::create()->build());
+        $params = ['index' => $this->argument('index name')];
+
+        $response = $client->getClient()->indices()->create($params);
+        $this->info("Index created successfully.");
+    }
+}
