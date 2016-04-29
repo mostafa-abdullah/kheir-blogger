8f9b4cdbacec1c84a080dcb76ea596849314ed41
Omarkojak <omarkojaks@gmail.com>
2016-04-29 10:19:41 +0200

    #133 | editing view of posts
    * adding hr and row tags to make the delete post button not touching
    with the other

--- resources/views/event/post/index.blade.php
+++ resources/views/event/post/index.blade.php
@@ -5,26 +5,29 @@
     @if(!count($event->posts))
      <h3 class="alert-info">This Event has no posts</h3>
     @else
-     @foreach($event->posts as $post)
-         <ul>
-             <li>
-                 @if($creator)
-                     @include('event.partials.button', ['buttonText' => 'Edit Post', 'action' => "post/".$post->id."/edit/"])
-                 @endif
-                 @if ($creator || (Auth::user() && Auth::user()->role >= 8))
-                     <form action="{{ url('event/'.$event->id."/post/".$post->id) }}" method="POST">
-                          {!! csrf_field() !!}
-                          {!! method_field('DELETE') !!}
-                          <button type="submit" class="btn btn-danger btn-event">Delete Post</button>
-                     </form>
-                @endif
-                 <h4>{{$post->title}}  <small>{{$post->created_at}}</small></h4>
-                 <p>
-                     {{$post->description}}
-                 </p>
+        <div class="row">
+            <hr>
+             @foreach($event->posts as $post)
+                 <ul>
+                     <li>
+                         @if($creator)
+                             @include('event.partials.button', ['buttonText' => 'Edit Post', 'action' => "post/".$post->id."/edit/"])
+                         @endif
+                         @if ($creator || (Auth::user() && Auth::user()->role >= 8))
+                             <form action="{{ url('event/'.$event->id."/post/".$post->id) }}" method="POST">
+                                  {!! csrf_field() !!}
+                                  {!! method_field('DELETE') !!}
+                                  <button type="submit" class="btn btn-danger btn-event">Delete Post</button>
+                             </form>
+                        @endif
+                         <h4>{{$post->title}}  <small>{{$post->created_at}}</small></h4>
+                         <p>
+                             {{$post->description}}
+                         </p>
 
-             </li>
-         </ul>
-     @endforeach
+                     </li>
+                 </ul>
+             @endforeach
+        </div>
     @endif
 </div>
